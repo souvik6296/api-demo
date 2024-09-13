b2dc85297744ca3f0f3c1379a8927fb248d485a0
Souvik Gupta <souvikgupta64@gmail.com>
2024-09-14 00:26:07 +0530

    upload

---
+++ server.js
@@ -0,0 +1,34 @@
+
+const express = require("express");
+const router = express.Router();
+const controler = require("../server2/controler/auth_controler");
+
+const app = express();
+
+
+app.use(express.json());
+
+const orgname = "Personal";
+
+const orgid = "org-QB2KomASwSYG4AZ9XSEhrRAs";
+
+
+router.route("/home").get(controler.home);
+router.route("/signup").post(controler.signup);
+router.route("/login").post(controler.login);
+
+
+app.use("/api", router);
+
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello Web-a-Thon');
+});
+
+const PORT = 4000;
+
+// app window
+
+app.listen(PORT, () => {
+    console.log(`Server is live at port: ${PORT}`)
+});
