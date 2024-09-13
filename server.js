0bdbe92ba4bd37d3a5c2a06a128f858cae26378e
Souvik Gupta <souvikgupta64@gmail.com>
2024-09-14 01:01:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*',        // Allows all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Allows all methods
+    allowedHeaders: ['Content-Type', 'Authorization']  // Specify allowed headers
+  }));
 
 
 router.route("/home").post(controler.home);
